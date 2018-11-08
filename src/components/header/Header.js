e42c0f899249714cbfd4dedbc5d4e6ee58410fd9
vbiaggi10 <vbiaggi10@gmail.com>
2018-11-07 19:56:28 -0500

    sin routes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
 import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Container } from 'mdbreact';
 
 
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Container } from 'mdbreact';
+
 
 class Main extends Component {
   render() {
     return (
-      <div>
-        <h1>Menu</h1>
+      <div className="view-height">
+        <Container>
+          <div>
+            <h1>Seguro hogar</h1>
+          </div>
+          <p>Esto no es un gasto, es una inversión.</p>
+        </Container>
       </div>
     );
   }
